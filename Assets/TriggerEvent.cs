f977fc5c863d2480ba985d65d1e8bbbd71571cec
kstanu3 <kstanu3@uic.edu>
2020-10-10 15:43:14 -0500

    Alpha

---
+++ Assets/TriggerEvent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public GameObject button;
+    Animator animation;
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+        button = GameObject.Find("Button");
+        button.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        button.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button Pressed");
+
+        animation.SetBool("TriggerEvent", true);
+        if (!audio.isPlaying)
+        {
+            audio.Play();
+        }
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button Released");
+
+        animation.SetBool("TriggerEvent", false);
+    }
+    
+
+}
